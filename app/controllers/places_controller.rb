989742972caed1a0dab4c1212b58465f0cb93f38
Jessica Lee <jessicayungalee@gmail.com>
2016-05-16 06:45:52 +0000

    Added the update function the places controller, now the edit button should be functional.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.page(params[:page]).per(10)
+    @places = Place.order("created_at ASC").page(params[:page]).per(10)
   end
 
   def new
@@ -20,6 +20,12 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+  end
+
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
   end
 
   def place_params
