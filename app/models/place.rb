88a9ad26293695408db7671bdedc55e7ddfda69b
Jessica Lee <jessicayungalee@gmail.com>
2016-05-18 14:25:52 +0000

    Added validations for address and description being present, as well as a 3 character minimum for the name field.

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,7 @@
 class Place < ActiveRecord::Base
   paginates_per 10
   belongs_to :user
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :address, presence: true
+  validates :description, presence: true
 end
