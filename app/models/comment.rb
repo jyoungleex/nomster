4ff294e1a58f92d9ac4a3ed5d9ff6b39eeafaaa9
Jessica Lee <jessicayungalee@gmail.com>
2016-06-06 21:33:51 +0000

    Fixed broken commits, created new git repository, added dropdown list for ratings.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,9 +4,13 @@ class Comment < ActiveRecord::Base
 
   RATINGS = {
     'one star'    => '1_star',
-    'two stars'   => '2_stars'
-    'three stars' => '3_stars'
+    'two stars'   => '2_stars',
+    'three stars' => '3_stars',
     'four stars'  => '4_stars',
     'five stars'  => '5_stars'
   }
+
+  def humanized_rating
+    RATINGS.invert[self.rating]
+  end
 end
